716f25f3bf3c28b3464412ce9538fcf5c9adbb69
ryan-craven <benjamin.r.craven@gmail.com>
2023-07-19 22:35:33 -0400

    Mostly automatic server config support

--- src/main/java/net/cravencraft/epicparagliders/UpdatedModCfg.java
+++ src/main/java/net/cravencraft/epicparagliders/UpdatedModCfg.java
@@ -1,49 +1,15 @@
 package net.cravencraft.epicparagliders;
 
-import com.google.common.collect.ImmutableList;
-import net.cravencraft.epicparagliders.capabilities.UpdatedPlayerState;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtIo;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Mth;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.predicate.BlockStatePredicate;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.*;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.server.ServerLifecycleHooks;
-import tictim.paraglider.ModCfg;
-import tictim.paraglider.ParagliderMod;
-import tictim.paraglider.capabilities.PlayerState;
-import tictim.paraglider.loot.ParagliderModifier;
 
-import javax.annotation.Nullable;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static net.cravencraft.epicparagliders.EpicParaglidersMod.MOD_ID;
 
-import static tictim.paraglider.ParagliderMod.MODID;
-
-@Mod.EventBusSubscriber(modid = MODID, bus = Bus.MOD)
+@Mod.EventBusSubscriber(modid = MOD_ID, bus = Bus.MOD)
 public final class UpdatedModCfg {
 	private UpdatedModCfg(){}
 	private static BooleanValue paraglidingConsumesStamina;
--- src/main/java/net/cravencraft/epicparagliders/events/EpicParaglidersEventHandler.java
+++ src/main/java/net/cravencraft/epicparagliders/events/EpicParaglidersEventHandler.java
@@ -3,12 +3,22 @@ package net.cravencraft.epicparagliders.events;
 import net.cravencraft.epicparagliders.EpicParaglidersMod;
 import net.cravencraft.epicparagliders.capabilities.UpdatedClientPlayerMovement;
 import net.cravencraft.epicparagliders.capabilities.UpdatedServerPlayerMovement;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.storage.WorldData;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import tictim.paraglider.capabilities.ClientPlayerMovement;
 import tictim.paraglider.capabilities.PlayerMovement;
 import tictim.paraglider.capabilities.ServerPlayerMovement;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = EpicParaglidersMod.MOD_ID)
 public final class EpicParaglidersEventHandler {
@@ -16,12 +26,68 @@ public final class EpicParaglidersEventHandler {
     private EpicParaglidersEventHandler() {}
 
     /**
+     * Looks up the paraglider-server.toml file in the serverconfig file, and changes the paraglidingConsumesStamina and
+     * runningAndSwimmingConsumesStamina attributes to false so that the epicparagliders-server.toml can properly
+     * override it. This should remove the need for players to manually configure the files themselves. Works for
+     * singleplayer and multiplayer worlds.
+     *
+     * TODO: Test in CurseForge. Brand new world.
+     *
+     * @param event The ServerStartEvent fires every time the server is started and ready to play.
+     */
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void testGetServerPath(ServerStartedEvent event) throws IOException {
+        MinecraftServer server = event.getServer();
+        WorldData worldData = server.getWorldData();
+
+        if (worldData != null) {
+            String worldName = worldData.getLevelName();
+            File paragliderConfig;
+
+            if (server.isSingleplayer()) {
+                EpicParaglidersMod.LOGGER.info("IS SINGLEPLAYER");
+                paragliderConfig = new File(server.getServerDirectory() + "/saves/" + worldName + "/serverconfig/paraglider-server.toml");
+            }
+            else {
+                paragliderConfig = new File(server.getServerDirectory() + "/" + worldName + "/serverconfig/paraglider-server.toml");
+            }
+
+            if (paragliderConfig.isFile()) {
+                EpicParaglidersMod.LOGGER.info("IS FILE");
+                modifyParagliderServerConfig(paragliderConfig);
+            }
+        }
+    }
+
+    /**
+     * Separate method that performs all the reading and modifying of the paraglider-server.toml file.
+     * Will look up the two attributes for paragliding and running to consume stamina and set them to false
+     * for EpicParagliders to take over.
+     *
+     * @param paragliderConfig the paraglider-server.toml file to be edited
+     * @throws IOException
+     */
+    private static void modifyParagliderServerConfig(File paragliderConfig) throws IOException {
+        List<String> fileContent = new ArrayList<>(Files.readAllLines(paragliderConfig.toPath(), StandardCharsets.UTF_8));
+
+        for (int i = 0; i < fileContent.size(); i++) {
+            if (fileContent.get(i).contains("paraglidingConsumesStamina = true") || fileContent.get(i).contains("runningAndSwimmingConsumesStamina = true")) {
+                EpicParaglidersMod.LOGGER.info(fileContent.get(i) + " || BEING CHANGED");
+                String overrideSetting = fileContent.get(i).replace("true", "false");
+                fileContent.set(i, overrideSetting);
+            }
+        }
+
+        Files.write(paragliderConfig.toPath(), fileContent, StandardCharsets.UTF_8);
+    }
+
+    /**
      * Instantiates the new UpdatedPlayerMovement classes. Both server side and client side.
      * Through a few different conditionals the classes are set to ensure that if the
      * Paragliders PlayerMovement classes are changed or null (death, logout, dimension change),
      * then the new UpdatedPlayerMovement classes are also changed.
      *
-     * @param event
+     * @param event The PlayerTick event fire for every tick both client and server side.
      */
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
