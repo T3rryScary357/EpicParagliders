8c31105ece41b293afc24f1f12f11986d79779c7
ryan-craven <benjamin.r.craven@gmail.com>
2023-11-11 00:05:12 -0500

    Dynamic stamina bar added. - Stamina bar will adjust its sized based on the max stamina to possibly achieve. - Notes on how to implement an Elden Ring like system added. Would like to look into that later.

--- src/main/java/net/cravencraft/epicparagliders/mixins/epicfight/client/BattleModeGuiMixin.java
+++ src/main/java/net/cravencraft/epicparagliders/mixins/epicfight/client/BattleModeGuiMixin.java
@@ -3,6 +3,7 @@ package net.cravencraft.epicparagliders.mixins.epicfight.client;
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.cravencraft.epicparagliders.EpicParaglidersMod;
 import net.cravencraft.epicparagliders.config.ConfigManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
@@ -16,9 +17,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tictim.paraglider.ModCfg;
 import tictim.paraglider.capabilities.ClientPlayerMovement;
 import tictim.paraglider.capabilities.PlayerMovement;
 import yesman.epicfight.api.utils.math.Vec2i;
+import yesman.epicfight.client.ClientEngine;
 import yesman.epicfight.client.gui.BattleModeGui;
 import yesman.epicfight.client.gui.EntityIndicator;
 import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerPatch;
@@ -26,9 +29,11 @@ import yesman.epicfight.config.ConfigurationIngame;
 
 @Mixin(BattleModeGui.class)
 public abstract class BattleModeGuiMixin extends GuiComponent {
-    @Shadow private int sliding;
-    @Shadow private boolean slidingToggle;
-    @Shadow @Final private ConfigurationIngame config;
+    @Shadow(remap = false) private int sliding;
+    @Shadow(remap = false) private boolean slidingToggle;
+    @Shadow(remap = false) @Final private ConfigurationIngame config;
+
+    private int maxPossibleStamina = ModCfg.maxStamina();
     private boolean decreaseOpacity;
     private float modifiedOpacity;
     private float ratio;
@@ -124,8 +129,8 @@ public abstract class BattleModeGuiMixin extends GuiComponent {
                 poseStack.pushPose();
                 poseStack.translate(0, this.sliding, 0);
                 this.setModifiedOpacity(1.0F, ratio, 0.25F, 1.0F);
-                GuiComponent.blit(poseStack, pos.x, pos.y, 118, 4, 2, 38, 237, 9, 255, 255);
-                GuiComponent.blit(poseStack, pos.x, pos.y, (int)(118*ratio), 4, 2, 47, (int)(237*ratio), 9, 255, 255);
+                GuiComponent.blit(poseStack, pos.x, pos.y, this.getDisplayStamina(118), 4, 2, 38, 237, 9, 255, 255);
+                GuiComponent.blit(poseStack, pos.x, pos.y, this.getDisplayStamina((int) (118*ratio)), 4, 2, 47, (int)(237*ratio), 9, 255, 255);
                 poseStack.popPose();
             }
             ci.cancel();
@@ -133,6 +138,31 @@ public abstract class BattleModeGuiMixin extends GuiComponent {
     }
 
     /**
+     * Adjusts the stamina bar's length to be relative to the max stamina a user has vs. the max
+     * stamina possible to achieve.
+     */
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiComponent;blit(Lcom/mojang/blaze3d/vertex/PoseStack;IIIIFFIIII)V", ordinal = 0), method = "renderGui")
+    private void modifyStaminaBarSize(PoseStack p_93161_, int p_93162_, int p_93163_, int p_93164_, int p_93165_, float p_93166_, float p_93167_, int p_93168_, int p_93169_, int p_93170_, int p_93171_) {
+        GuiComponent.blit(p_93161_, p_93162_, p_93163_, this.getDisplayStamina(p_93164_), p_93165_, p_93166_, p_93167_, p_93168_, p_93169_, p_93170_, p_93171_);
+    }
+
+    /**
+     * Adjusts the stamina bar's dynamic length to be relative to the max stamina a user has vs. the max
+     * stamina possible to achieve.
+     */
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiComponent;blit(Lcom/mojang/blaze3d/vertex/PoseStack;IIIIFFIIII)V", ordinal = 1), method = "renderGui")
+    private void modifyDynamicStaminaBar(PoseStack p_93161_, int p_93162_, int p_93163_, int p_93164_, int p_93165_, float p_93166_, float p_93167_, int p_93168_, int p_93169_, int p_93170_, int p_93171_) {
+        GuiComponent.blit(p_93161_, p_93162_, p_93163_, this.getDisplayStamina(p_93164_), p_93165_, p_93166_, p_93167_, p_93168_, p_93169_, p_93170_, p_93171_);
+    }
+
+    /**
+     * TODO: THIS WORKS! I'd like to create one extra config for an Elden Ring-like stamina system.
+     *       Stamina only drains while running/swimming if you are in battle mode. Makes traversal
+     *       less tedious, but still keeps combat intense. Sure, it can be abused by just simply turning
+     *       battle mode off and running away a bit, but I can mitigate that with a counter that takes
+     *       at least 3-5 seconds to stop consuming stamina after switching. Should discourage abusing that.
+     *
+     *
      * Modifies the Epic Fight stamina bar. It will now turn red and the opacity will decrease whenever
      * the player's stamina is depleted.
      *
@@ -145,6 +175,10 @@ public abstract class BattleModeGuiMixin extends GuiComponent {
             method = "renderGui")
     private void modifyStaminaBar(float red, float blue, float green, float opacity) {
         this.setModifiedOpacity(red, this.ratio, green, opacity);
+    }
+
+    private int getDisplayStamina(int originalStamina) {
+        return (int) (originalStamina / (this.maxPossibleStamina / ClientEngine.getInstance().getPlayerPatch().getMaxStamina()));
     }
 
     /**
