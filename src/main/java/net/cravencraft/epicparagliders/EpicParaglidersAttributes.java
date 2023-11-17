0ac7146772c087393eb37bf9ed138f79512ad5e9
ryan-craven <benjamin.r.craven@gmail.com>
2023-11-17 00:52:58 -0500

    Bare bones attribute support

--- src/main/java/net/cravencraft/epicparagliders/EpicParaglidersAttributes.java
+++ src/main/java/net/cravencraft/epicparagliders/EpicParaglidersAttributes.java
@@ -12,9 +12,34 @@ public class EpicParaglidersAttributes {
     public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, EpicParaglidersMod.MOD_ID);
 
     /**
-     * TODO: Attributes for stats
+     * Weapon Attributes
      */
-    public static final RegistryObject<Attribute> DAGGER_STAMINA_REDUCTION = ATTRIBUTES.register("dagger_stamina_reduction", () -> new RangedAttribute("dagger_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> DAGGER_STAMINA_REDUCTION = ATTRIBUTES.register("dagger_stamina_reduction", () ->
+            new RangedAttribute("dagger_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> SWORD_STAMINA_REDUCTION = ATTRIBUTES.register("sword_stamina_reduction", () ->
+            new RangedAttribute("sword_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> LONGSWORD_STAMINA_REDUCTION = ATTRIBUTES.register("longsword_stamina_reduction", () ->
+            new RangedAttribute("longsword_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> GREATSWORD_STAMINA_REDUCTION = ATTRIBUTES.register("greatsword_stamina_reduction", () ->
+            new RangedAttribute("greatsword_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> UCHIGATANA_STAMINA_REDUCTION = ATTRIBUTES.register("uchigatana_stamina_reduction", () ->
+            new RangedAttribute("uchigatana_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> TACHI_STAMINA_REDUCTION = ATTRIBUTES.register("tachi_stamina_reduction", () ->
+            new RangedAttribute("tachi_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> SPEAR_STAMINA_REDUCTION = ATTRIBUTES.register("spear_stamina_reduction", () ->
+            new RangedAttribute("spear_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> KNUCKLE_STAMINA_REDUCTION = ATTRIBUTES.register("knuckle_stamina_reduction", () ->
+            new RangedAttribute("knuckle_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> AXE_STAMINA_REDUCTION = ATTRIBUTES.register("axe_stamina_reduction", () ->
+            new RangedAttribute("axe_stamina_reduction", 1.0, 0.0, 10.0));
+
+    /**
+     * Skill Attributes
+     */
+    public static final RegistryObject<Attribute> BLOCK_STAMINA_REDUCTION = ATTRIBUTES.register("block_stamina_reduction", () ->
+            new RangedAttribute("block_stamina_reduction", 1.0, 0.0, 10.0));
+    public static final RegistryObject<Attribute> DODGE_STAMINA_REDUCTION = ATTRIBUTES.register("dodge_stamina_reduction", () ->
+            new RangedAttribute("dodge_stamina_reduction", 1.0, 0.0, 10.0));
 
     /**
      * Attributes needed for datapack editing
